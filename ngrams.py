d70ae7117627cc3c1d235ac55917a65f2e974fe9
Joel Wilkins <Joel N Wilkins>
2018-09-27 15:03:02 +0100

    Replaced the ngram iterator with a generator expression

--- ngrams.py
+++ ngrams.py
@@ -1,18 +1,3 @@
-class NGrams:
-    def __init__(self, text, n):
-        self.text = text
-        self.n = n
-        if len(self.text) < self.n:
-            raise ValueError("the length of the text must be larger than n")
-
-    def __iter__(self):
-        self.count = -1
-        self.limit = len(self.text) - self.n + 1
-        return self
-
-    def __next__(self):
-        self.count += 1
-        if self.count < self.limit:
-            return self.text[self.count:self.count+self.n]
-        else:
-            raise StopIteration()
+def NGrams(text, n):
+    for i in range(len(text)-n+1):
+        yield text[i:i+n]
