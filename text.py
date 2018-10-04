8b8132aefe431e21951b53cbd27a84fe440576c7
Joel Wilkins <Joel N Wilkins>
2018-10-04 16:18:51 +0100

    Optimised Text object

--- text.py
+++ text.py
@@ -19,21 +19,21 @@ class Text:
             self.__set__(*args, **kwargs)
 
     def __repr__(self):
-        if len(self.__text) > 70:
-            return "Text(\"{}...\")".format("".join(self.__text[:70]))
-        return "Text(\"{}\")".format(self.__str__())
+        if len(self.__text) > 79:
+            return "{}(\"{}...\")".format(self.__class__.__name__, self.__text[:76])
+        return "{}(\"{}\")".format(self.__class__.__name__, self.__text)
 
     def __str__(self):
-        return "".join(self.__text)
+        return self.__text
 
     def __get__(self):
-        return self.__str__()
+        return self.__text
 
     def __getitem__(self, key):
         if isinstance(key, int):
             return self.__text[key]
         elif isinstance(key, slice):
-            return "".join(self.__text[key.start:key.stop])
+            return (self.__text[key.start:key.stop]
 
     def __set__(self, *args, **kwargs):
         if len(args) == 1:
@@ -42,17 +42,20 @@ class Text:
                 text = text.upper()
             elif self.__case == "LOWER":
                 text = text.lower()
-            self.__text = [char for char in text]
+            self.__text = "".join([char for char in text])
         else:
             print(args, kwargs)
             raise ValueError("{} only takes one argument".format(self.__class__.__name__))
 
-    def __setitem__(self, char1, char2):
-        for i, char in enumerate(self.__text):
-            if char == char1:
-                self.__text[i] = char2
-            elif char == char2:
-                self.__text[i] = char1
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            self.__text[key] = value
+        elif isinstance(key, str):
+            for i, char in enumerate(self.__text):
+                if char == key:
+                    self.__text[i] = value
+                elif char == value:
+                    self.__text[i] = key
 
     def __len__(self):
         return len(self.__text)
@@ -61,13 +64,13 @@ class Text:
         self.__setitem__(char1, char2)
 
     def score(self):
-        return self.__score.score(self.__str__())
+        return self.__score.score(self.__text)
 
     def copy(self):
-        return Text(self.__str__())
+        return Text(self.__text)
 
     def reverse(self):
-        self.__text = list(reversed(self.__text))
+        self.__text = "".join(list(reversed(self.__text)))
 
     def reverse_words(self, sep=" "):
-        self.__text = list(sep.join(["".join(list(reversed(word))) for word in self.__str__().split(sep)]))
+        self.__text = sep.join(["".join(list(reversed(word))) for word in self.__text.split(sep)])
