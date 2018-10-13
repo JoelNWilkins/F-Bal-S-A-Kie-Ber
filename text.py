152f9269c01ee328077437834c03c1a7fc242003
Joel Wilkins <Joel N Wilkins>
2018-10-13 23:56:19 +0100

    Modified Text class

--- text.py
+++ text.py
@@ -1,41 +1,50 @@
 import re
 import string
 
-def ngrams(text, n):
+def ngrams(text, n, overlap=True):
     if len(text) < n:
         raise ValueError("the length of the text must be larger than n")
-    for i in range(len(text)-n+1):
-        yield text[i:i+n]
+    if overlap:
+        for i in range(len(text)-n+1):
+            yield text[i:i+n]
+    elif not overlap:
+        for i in range(int(len(text)//n)):
+            yield text[i*n:(i+1)*n]
 
 class Text:
-    def __init__(self, *args, **kwargs):
-        if "chars" in kwargs.keys():
-            self.__chars = kwargs.pop("chars")
-        else:
-            self.__chars = string.ascii_uppercase
+    def __init__(self, text, chars=string.ascii_uppercase, case="auto"):
+        self.__chars = chars
+        self.__case = case.lower()
 
-        if self.__chars == string.ascii_uppercase:
-            text = args[0].upper()
-        elif self.__chars == string.ascii_lowercase:
-            text = args[0].lower()
-        else:
-            text = args[0]
+        if self.__case == "auto":
+            if self.__chars == string.ascii_uppercase:
+                text = text.upper()
+            elif self.__chars == string.ascii_lowercase:
+                text = text.lower()
+        elif self.__case == "upper":
+            text = text.upper()
+        elif self.__case == "lower":
+            text = text.lower()
 
         self.__text = [char for char in text if char in self.__chars]
         p = re.compile("[{}]".format(self.__chars))
-        self.__temp = "{}".join(p.split(args[0]))
+        self.__temp = "{}".join(p.split(text))
 
     def __repr__(self):
         return "".join(self.__text)
 
-    def __str__(self, *args, **kwargs):
+    def __str__(self):
         return self.__temp.format(*self.__text)
 
     def __get__(self):
         return self.__repr__()
 
     def __getitem__(self, key):
-        return self.__text[key]
+        if isinstance(key, int) or isinstance(key, slice):
+            return self.__text[key]
+        else:
+            raise TypeError("{} indices must be integers or slices, not {}"
+                .format(self.__class__.__name__, key.__class__.__name__))
 
     def __setitem__(self, key, value):
         if isinstance(key, str):
@@ -47,7 +56,8 @@ class Text:
         elif isinstance(key, int) or isinstance(key, slice):
             self.__text[key] = value
         else:
-            raise TypeError("invalid index of type '{}'".format(key.__class__.__name__))
+            raise TypeError("{} indices must be integers, slices or strings, not {}"
+                .format(self.__class__.__name__, key.__class__.__name__))
 
     def __len__(self):
         return len(self.__text)
@@ -69,11 +79,12 @@ class Text:
         self.__text = list(reversed(self.__text))
 
     def reverse_words(self, sep=" "):
-        self.__text = sep.join(["".join(list(reversed(word))) for word in self.__repr__().split(sep)])
+        self.__text = sep.join(["".join(list(reversed(word)))
+            for word in self.__repr__().split(sep)])
 
     def shift(self, n):
         self.__text = "".join([self.__chars[(self.__chars.index(char)+n) % len(self.__chars)]
             for char in self.__text])
 
-    def ngrams(self, n):
-        return ngrams(self.__repr__(), n)
+    def ngrams(self, n, overlap=True):
+        return ngrams(self.__repr__(), n, overlap=overlap)
